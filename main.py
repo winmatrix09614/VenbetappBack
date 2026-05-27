164120e6a88d3cc5d0cd47c67d58e89ce056d16c
WinMatrix <koks.dm@gmail.com>
2026-05-27 15:19:49 +0500

    allow *

--- main.py
+++ main.py
@@ -461,7 +461,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://venbetapp-production.up.railway.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
